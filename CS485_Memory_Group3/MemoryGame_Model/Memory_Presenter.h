33fafc5d4a2f0701252b49be8a25c208fd2e5e61
lemo9842 <lemo9842@pacificu.edu>
2019-04-17 17:01:36 -0700

    Update Memory_Presenter.h

--- CS485_Memory_Group3/MemoryGame_Model/Memory_Presenter.h
+++ CS485_Memory_Group3/MemoryGame_Model/Memory_Presenter.h
@@ -1,1 +1,33 @@
 #pragma once
+#include <string>
+#include "Model.h"
+
+class IMemory_View;
+
+class Memory_Presenter
+{
+public:
+	Memory_Presenter(IMemory_View* pcView, Model* pcModel);
+	~Memory_Presenter();
+
+	virtual void setPlayer1Name(std::string szName);
+	virtual void setPlayer2Name(std::string szName);
+	virtual void flip(int x, int y);
+	virtual void nextTurn();
+
+	/*
+	+Memory_Presenter(IMemory_View*,Model*)
++~Memory_Presenter()
+
+//funcs called by view
+/+setPlayer1Name(string):void/
+/+setPlayer2Name(string):void/
+/+flip(int,int):void/
+/+nextTurn():void/
+	*/
+
+private:
+	Model *mpcModel;
+	IMemory_View *mpcView;
+};
+
